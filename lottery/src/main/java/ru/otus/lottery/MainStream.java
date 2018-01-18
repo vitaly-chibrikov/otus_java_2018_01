151aa160cbb0e6545bc2e0ad652f5dba4bce2ad4
vitaly.chibrikov <vitaly.chibrikov@gmail.com>
2018-01-18 21:35:46 +0300

    names

--- lottery/src/main/java/ru/otus/lottery/MainStream.java
+++ lottery/src/main/java/ru/otus/lottery/MainStream.java
@@ -7,11 +7,24 @@ import java.util.Comparator;
 
 /*
 https://github.com/vitaly-chibrikov/otus_java_2018_01
+
+удалите строку .map(line -> line.substring(0, line.indexOf("@")))
+пишите параметром запуска файл source.csv
+запускайте
 */
 
 public class MainStream {
     public static void main(String[] args) throws IOException {
-        String salt = "The Force will be with you. Always.";
+        String salt = "Иван Волков\u200BI'll\uFEFF be back)\n" +
+                "\n" +
+                "\n" +
+                "Владимир Лиханов\u200B42\uFEFF\n" +
+                "\n" +
+                "Ярослав Калашник\u200B))) ну ок, допустим\uFEFF\n" +
+                "\n" +
+                "Senior Tomato\u200BThe Gods will be\uFEFF with us\n" +
+                "\n" +
+                "Бари Хафизуллин\u200Byou shall\uFEFF not pass";
 
         Files.lines(Paths.get(args[0]))
                 .map(line -> line.replace("\"", ""))
