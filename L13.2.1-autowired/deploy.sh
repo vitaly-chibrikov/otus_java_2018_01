2d7ed8324899054974e23389019a7dbe64611a4c
Vitaly Chibrikov <vitaly.chibrikov@gmail.com>
2018-05-16 19:51:19 +0300

    Spring context + loggers

---
+++ L13.2.1-autowired/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mvn clean package
+
+# linux
+# cp target/L13.2.1-autowired.war ~/apps/jetty/webapps/root.war
+
+# windows
+# copy target\L13.2.1-autowired.war c:\Apps\Jetty\webapps\root.war
