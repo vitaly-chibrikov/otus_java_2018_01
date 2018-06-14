7629d9eb1832a4d56c5e139dd596e8b8f7c50f76
Vitaly Chibrikov <vitaly.chibrikov@gmail.com>
2018-06-14 19:53:25 +0300

    NIO

---
+++ L16.2.1-server/src/main/java/ru/otus/l161/app/MsgWorker.java
@@ -0,0 +1,17 @@
+package ru.otus.l161.app;
+
+import java.io.IOException;
+
+/**
+ * Created by tully.
+ */
+public interface MsgWorker {
+    void send(Msg msg);
+
+    Msg pool();
+
+    @Blocks
+    Msg take() throws InterruptedException;
+
+    void close() throws IOException;
+}
